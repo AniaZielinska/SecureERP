02e2635e4f2dda5f8cdd2c1ad37112bf17838e5e
AniaZielinska <145710604+AniaZielinska@users.noreply.github.com>
2023-11-06 17:45:43 +0100

    Add files via upload

---
+++ view/terminal.py
@@ -0,0 +1,58 @@
+def print_menu(title, list_options):
+    if len(list_options) > 1:
+        print(f"\n{title}")
+        for i in range(len(list_options)-1):
+            print(f"({i+1}) {list_options[i+1]}")
+        print(f"(0) {list_options[0]}")
+
+
+def print_message(message):
+    """Prints a single message to the terminal.
+
+    Args:
+        message: str - the message
+    """
+    pass
+
+
+def print_general_results(result, label):
+    """Prints out any type of non-tabular data.
+    It should print numbers (like "@label: @value", floats with 2 digits after the decimal),
+    lists/tuples (like "@label: \n  @item1; @item2"), and dictionaries
+    (like "@label \n  @key1: @value1; @key2: @value2")
+    """
+    pass
+
+
+# /--------------------------------\
+# |   id   |   product  |   type   |
+# |--------|------------|----------|
+# |   0    |  Bazooka   | portable |
+# |--------|------------|----------|
+# |   1    | Sidewinder | missile  |
+# \-----------------------------------/
+def print_table(table):
+    """Prints tabular data like above.
+
+    Args:
+        table: list of lists - the table to print out
+    """
+    pass
+
+
+def get_input(label):
+    user_option = input(f"{label}: ")
+    return user_option
+
+
+def get_inputs(labels):
+    """Gets a list of string inputs from the user.
+
+    Args:
+        labels: list - the list of the labels to be displayed before each prompt
+    """
+    pass
+
+
+def print_error_message(message):
+    print(f"\n{message}")
